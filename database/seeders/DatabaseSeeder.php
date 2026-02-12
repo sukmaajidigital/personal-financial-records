f5ae3a87589f3b6b80be0ff16fc2fbd1436d71c2
ags <114824269+sukmaajidigital@users.noreply.github.com>
2026-02-13 01:13:39 +0700

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call([
             CategorySeeder::class,
             TransactionSeeder::class,
